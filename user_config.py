775ed2265628b6bae4222a8dd5153f3406857927
stamplevskiyd <stamplevskiyd@gmail.com>
2024-05-22 22:42:40 +0300

    Standard user config

--- user_config.py
+++ user_config.py
@@ -6,6 +6,8 @@ sent_file: str = "sent.pcap"
 # File with received packets
 received_file: str = "received.pcap"
 
+sent_file = received_file = "traffic_00003_20230906061206.pcap"
+
 # folder where to save packet groups for sent packets
 sent_groups_folder: str = "sent_groups"
 
@@ -13,14 +15,14 @@ sent_groups_folder: str = "sent_groups"
 received_groups_folder: str = "received_groups"
 
 # Multiprocessing settings
-use_multiprocessing: bool = False
+use_multiprocessing: bool = True
 proc_count = 8
 
 # Mode: standard or tokenized
 string_algorithm: StringAlgorithmType = StringAlgorithmType.standard
 
 # Key fields
-# selected_key_fields: dict[str, list[str]] = {
-#     "Ethernet": ["type"],
-#     "IP": ["version", "ihl", "len", "id", "frag", "proto"]
-# }
+selected_key_fields: dict[str, list[str]] = {
+    "Ethernet": ["type"],
+    "IP": ["version", "ihl", "len", "id", "frag", "proto"],
+}
